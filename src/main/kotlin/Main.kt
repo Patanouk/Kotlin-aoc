7c90d3fd672bd5d38846ae26906644b18360d794
Jean Lainee <jean.lainee@gmail.com>
2023-12-04 01:32:26 +0800

    Solve Aoc2023Day3 second star

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,5 @@ import day2.Aoc2023Day2
 import day3.Aoc2023Day3
 
 fun main() {
-    println(Aoc2023Day3.solveFirstStar())
+    println(Aoc2023Day3.solveSecondStar())
 }
--- src/main/kotlin/day3/Aoc2023Day3.kt
+++ src/main/kotlin/day3/Aoc2023Day3.kt
@@ -14,15 +14,16 @@ object Aoc2023Day3 {
 
         for (i in input.indices) {
             for (j in input[0].indices) {
-                if (input[i][j].isDigit()) {
-                    currentNumberCoordinates.add(Coordinates(i, j))
-                    currentNumber = currentNumber * 10 + input[i][j].digitToInt()
-                }
+
+                input[i][j]
+                    .takeIf { it.isDigit() }
+                    ?.let { currentNumberCoordinates.add(Coordinates(i, j)) }
+                    ?.let { currentNumber = currentNumber * 10 + input[i][j].digitToInt() }
 
                 // We're at the end of the line, or the current input is not a digit
                 if (j == input[0].size - 1 || !input[i][j].isDigit()) {
                     currentNumberCoordinates
-                        .takeIf { coordinates -> coordinates.any {hasSymbolNeighbour(input, it)} }
+                        .takeIf { coordinates -> coordinates.any { hasSymbolNeighbour(input, it) } }
                         ?.let { result += currentNumber }
 
                     currentNumber = 0
@@ -52,8 +53,68 @@ object Aoc2023Day3 {
         return false
     }
 
+    fun solveSecondStar(): Int {
+        val input = readInput("/day3/input.txt")
+            .map { it.toCharArray().toList() }
+        var result = 0
+
+        var currentNumber = 0
+        val currentNumberCoordinates = mutableListOf<Coordinates>()
+        val coordinatesToNumber = mutableListOf<NumberWithCoordinates>()
+
+        for (i in input.indices) {
+            for (j in input[0].indices) {
+
+                input[i][j]
+                    .takeIf { it.isDigit() }
+                    ?.let { currentNumberCoordinates.add(Coordinates(i, j)) }
+                    ?.let { currentNumber = currentNumber * 10 + input[i][j].digitToInt() }
+
+                currentNumberCoordinates
+                    .takeIf { j == input[0].size - 1 || !input[i][j].isDigit() }
+                    ?.takeIf { currentNumber != 0 }
+                    ?.let { coordinatesToNumber.add(NumberWithCoordinates(currentNumber, it.toList())) }
+                    ?.also {
+                        currentNumber = 0
+                        currentNumberCoordinates.clear()
+                    }
+            }
+        }
+
+        for (i in input.indices) {
+            for (j in input[0].indices) {
+                result += input[i][j]
+                    .takeIf { it == '*' }
+                    ?.let { getNeighbouringNumbers(coordinatesToNumber, Coordinates(i, j)) }
+                    ?.takeIf { it.size == 2 }
+                    ?.let { it[0] * it[1] }
+                    ?: 0
+            }
+        }
+
+        return result
+    }
+
+    private fun getNeighbouringNumbers(coordinatesToNumber: List<NumberWithCoordinates>, gearCoordinate: Coordinates): List<Int> {
+        return coordinatesToNumber
+            .filter { it.coordinates.any { coordinate ->  coordinate.isAdjacent(gearCoordinate) } }
+            .map { it.number }
+    }
+
+    data class NumberWithCoordinates(
+        val number: Int,
+        val coordinates: List<Coordinates>
+    )
+
     data class Coordinates(
         val i: Int,
         val j: Int,
-    )
+    ) {
+        fun isAdjacent(c: Coordinates): Boolean {
+            return this.i >= c.i - 1
+                    && this.i <= c.i + 1
+                    && this.j >= c.j - 1
+                    && this.j <= c.j + 1
+        }
+    }
 }
