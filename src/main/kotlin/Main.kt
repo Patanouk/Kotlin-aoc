7226cbac04584fa0216b03af377539e9f7067e7d
Jean Lainee <jean.lainee@gmail.com>
2023-12-05 23:17:40 +0800

    Solve Aoc2023Day5 second star

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
-import day4.Aoc2023Day4
 import day5.Aoc2023Day5
 
 fun main() {
-    println(Aoc2023Day5.solveFirstStar())
+    println(Aoc2023Day5.solveSecondStar())
 }
--- src/main/kotlin/day5/Aoc2023Day5.kt
+++ src/main/kotlin/day5/Aoc2023Day5.kt
@@ -47,21 +47,80 @@ object Aoc2023Day5 {
         return nextSeeds.union(previousSeeds).min()
     }
 
+    fun solveSecondStar(): Long {
+        val input = readInput("/day5/input.txt")
 
+        val seeds = input.first()
+            .substringAfter("seeds: ")
+            .trim()
+            .split("\\s+".toRegex())
+            .map { it.toLong() }
+            .chunked(2)
+            .map { LongRange(it[0], it[0] + it[1] - 1) }
+            .toSet()
+
+        val previousRanges = seeds.toMutableSet()
+        val nextRanges = mutableSetOf<LongRange>()
+
+        for (line in input.subList(2, input.size)) {
+            if (line.endsWith("map:")) {
+                continue
+            }
+
+            if (line.isEmpty()) {
+                nextRanges.addAll(previousRanges)
+                previousRanges.clear()
+
+                previousRanges.addAll(nextRanges)
+                nextRanges.clear()
+                continue
+            }
+
+            val rangeWithDestination = line.trim()
+                .split("\\s+".toRegex())
+                .map { it.toLong() }
+                .let { RangeWithDestination(sourceStart = it[1], destinationStart = it[0], length = it[2]) }
+
+            val previousRangesToRemove = mutableSetOf<LongRange>()
+            val previousRangesToAdd = mutableSetOf<LongRange>()
+
+            previousRanges.forEach{ previousRange ->
+                if (rangeWithDestination.contains(previousRange.first) || rangeWithDestination.contains(previousRange.last)) {
+                    val minRangeToMap = previousRange.first.coerceAtLeast(rangeWithDestination.sourceStart)
+                    val maxRangeToMap = previousRange.last.coerceAtMost(rangeWithDestination.sourceEnd)
+
+                    nextRanges.add(LongRange(
+                        rangeWithDestination.destinationStart + (minRangeToMap - rangeWithDestination.sourceStart),
+                        rangeWithDestination.destinationStart + (maxRangeToMap - rangeWithDestination.sourceStart),
+                    ))
+
+                    previousRangesToRemove.add(previousRange)
+                    if (previousRange.first < rangeWithDestination.sourceStart)
+                        previousRangesToAdd.add(LongRange(previousRange.first, rangeWithDestination.sourceStart - 1))
+
+                    if (previousRange.last > rangeWithDestination.sourceEnd)
+                        previousRangesToAdd.add(LongRange(rangeWithDestination.sourceEnd + 1, previousRange.last))
+                }
+            }
+
+            previousRanges.removeAll(previousRangesToRemove.toSet())
+            previousRanges.addAll(previousRangesToAdd)
+        }
+
+        return nextRanges.union(previousRanges).minOf { it.first }
+    }
 
     private data class RangeWithDestination(
         val sourceStart: Long,
         val destinationStart: Long,
         val length: Long,
     ) {
+        val sourceEnd: Long = sourceStart + length - 1
+
         fun contains(value: Long): Boolean {
             return sourceStart <= value && value < sourceStart + length
         }
 
-        fun getNewValue(value: Long): Long {
-            return destinationStart + (value - sourceStart)
-        }
-
+        fun getNewValue(value: Long)= destinationStart + (value - sourceStart)
     }
-
 }
