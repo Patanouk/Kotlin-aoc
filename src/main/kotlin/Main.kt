5cdf047a336c132e32ea575c5f96850159b1c82a
Jean Lainee <jean.lainee@gmail.com>
2023-12-08 19:51:01 +0800

    Solve Aoc2023Day8 second star brute force

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 import day7.Aoc2023Day7
 
 fun main() {
-    println(day8.Aoc2023Day8.solveFirstStar())
+    println(day8.Aoc2023Day8.solveSecondStar())
 }
--- src/main/kotlin/day8/Aoc2023Day8.kt
+++ src/main/kotlin/day8/Aoc2023Day8.kt
@@ -17,16 +17,38 @@ object Aoc2023Day8 {
         var numberOfSteps = 0
 
         while (currentNode != "ZZZ") {
-            currentNode = when (val direction = directions[numberOfSteps % directions.length]) {
-                'L' -> nodes[currentNode]!!.left
-                'R' -> nodes[currentNode]!!.right
-                else -> throw IllegalArgumentException("Does not support direction $direction")
-            }
-            numberOfSteps ++
+            currentNode = getNextNode(currentNode, nodes, directions, numberOfSteps)
+            numberOfSteps++
         }
 
         return numberOfSteps
     }
+
+    fun solveSecondStar(): Int {
+        val input = readInput("/day8/input.txt")
+        val directions = input.first()
+
+        val nodes = input.subList(2, input.size)
+            .map { it.split(" = ") }.associate { Pair(it[0], Node.build(it[1])) }
+
+        var currentNodes = nodes.keys.filter { it.endsWith('A') }
+        var numberOfSteps = 0
+
+        while (!currentNodes.all { it.endsWith('Z') }) {
+            currentNodes = currentNodes.map { getNextNode(it, nodes, directions, numberOfSteps) }
+            numberOfSteps++
+            println(numberOfSteps)
+        }
+
+        return numberOfSteps
+    }
+
+    private fun getNextNode(currentNode: String, nodes: Map<String, Node>, directions: String, numberOfSteps: Int) =
+        when (val direction = directions[numberOfSteps % directions.length]) {
+            'L' -> nodes[currentNode]!!.left
+            'R' -> nodes[currentNode]!!.right
+            else -> throw IllegalArgumentException("Does not support direction $direction")
+        }
 
     private data class Node(
         val left: String,
